1054a4358f14b45a4bb69e3b871932635315ab06
Kolapo Oni <onikolapo@gmail.com>
2019-03-13 08:05:33 +0100

    updated MainPage style & style_general.scss

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -20,38 +20,7 @@ export default {
 
 <style lang="scss" scoped>
 #main-page{
-  padding-top: 38px;
-}
-
-#top-nav{
   width: 90%;
   margin: 0 auto;
-  border: solid 1px #000000;
-  
-  .top-section, .bottom-section{
-    width: 100%;
-    display: flex;
-  }
-
-  .top-section{
-    border-bottom: solid 1px #000000;
-  }
-
-  .nav-item, .product-title{
-    width: 25%;
-    background: bisque;
-    border-right: solid 1px #000000;
-    padding-top: 25px;
-    padding-bottom: 25px;
-    text-align: center;
-    // float: left;
-  }
-
-  .product-title{
-    width: 50%;
-    font-size: 32px;
-    padding-top: 15px;
-    // float: left;
-  }
 }
 </style>

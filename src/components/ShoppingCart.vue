8226a584cd9f2a842200446f4e0bc75c231a51ec
Kolapo Oni <onikolapo@gmail.com>
2019-03-14 09:53:54 +0100

    created ShoppingCart component

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cart-section">
+    <div class="header-text-28">Shopping Cart</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShoppingCart',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

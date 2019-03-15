323facc3337a2fd529cff5ff0d56b407f81fa7f2
Kolapo Oni <onikolapo@gmail.com>
2019-03-15 11:28:09 +0100

    added login, signup and checkout page to routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,9 @@ import BuyPower from '@/components/BuyPower';
 import BuyProducts from '@/components/BuyProducts';
 import ProductView from '@/components/ProductView';
 import ShoppingCart from '@/components/ShoppingCart';
+import Login from '@/components/Login';
+import Signup from '@/components/Signup';
+import CheckoutPage from '@/CheckoutPage'
 
 Vue.use(Router)
 
@@ -12,14 +15,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'MainPage',
       component: MainPage,
       children: [
         { path: '', component: BuyPower },
         { path: 'product-catalogue', component: BuyProducts },
         { path: 'product', component: ProductView },
-        { path: 'cart', component: ShoppingCart }
+        { path: 'cart', component: ShoppingCart },
+        { path: 'Login', component: Login },
+        { path: 'Signup', component: Signup }
       ]
+    },
+    {
+      path: '/checkout',
+      name: 'CheckoutPage',
+      component: CheckoutPage,
     }
   ]
 })

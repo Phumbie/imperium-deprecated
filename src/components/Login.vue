a1e68155f63a3ef59c01cf5a3e277ce056ffaed9
Kolapo Oni <onikolapo@gmail.com>
2019-03-15 11:27:27 +0100

    created login and signup components

---
+++ src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="login-section">
+    <form>
+      <div class="header-text-28">Login</div>
+      <input type="text" placeholder="Email address" class="text-field">
+      <input type="text" placeholder="Password" class="text-field">
+      <div class="buttons">
+        <button class="border-none">Forgot your password?</button>
+        <button class="border-none" @click="goTo('/signup')">Create account</button>
+        <button>Sign in</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+    goTo(page) {
+      this.$router.push(page); 
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#login-section{
+ 
+}
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="signup-section">
+    <form>
+      <div class="header-text-28">Signup</div>
+      <input type="text" placeholder="First name" class="text-field">
+      <input type="text" placeholder="Last name" class="text-field">
+      <input type="text" placeholder="Email address" class="text-field">
+      <input type="text" placeholder="Password" class="text-field">
+      <div class="buttons">
+        <button>Create account</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#signup-section{
+ 
+}
+</style>

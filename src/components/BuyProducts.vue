33a792bf07c669653d703794dfe3c8bec7b5f642
Kolapo Oni <onikolapo@gmail.com>
2019-03-15 11:37:38 +0100

    added click functionality to product items to link to ProductView

--- src/components/BuyProducts.vue
+++ src/components/BuyProducts.vue
@@ -3,9 +3,11 @@
     <div class="header-text-28">Power your home with our range of products</div> 
     <div class="small-text">Find batteries, inverters, solar panels and more</div>
     <div class="products-container">
-      <div class="product-item" v-for="(n, index) in 12" :key="index">
+      <div class="product-item" v-for="(n, index) in 12" :key="index" @click="goTo('/product')">
         <div class="centered-content">
-          <div class="image-container"></div>
+          <div class="image-container">
+            <img src="../assets/images/battery.jpg">
+          </div>
           <div class="small-text-18">10kWh Battery</div>
           <div class="small-text-18 text-blue">3 configurations</div>
           <div class="small-text-18 text-blue">Suggested loading</div>
@@ -17,22 +19,25 @@
 
 <script>
 export default {
-  name: 'TopNav',
   data () {
     return {
     }
+  },
+
+  methods: {
+    goTo(page) {
+      this.$router.push(page); 
+    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
 #normal-products-section{
-  padding-top: 36px;
-  padding-bottom: 70px;
-  display: flex;
-  flex-direction: column;
 
   .product-item{
+    cursor: pointer;
+
     .centered-content{
       width: 70%;
     }

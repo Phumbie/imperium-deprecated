9c0dc2d32716a2f1169e5066f0129abfb3a376bb
Kolapo Oni <onikolapo@gmail.com>
2019-05-17 19:38:26 +0100

    updated signup form fields

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,10 +2,15 @@
   <div id="signup-section">
     <form>
       <div class="header-text-28">Signup</div>
-      <input type="text" placeholder="First name" class="text-field">
-      <input type="text" placeholder="Last name" class="text-field">
-      <input type="text" placeholder="Email address" class="text-field">
-      <input type="password" placeholder="Password" class="text-field">
+      <input type="text" placeholder="First name" class="text-field width-half" required>
+      <input type="text" placeholder="Last name" class="text-field width-half float-right" required>
+      <input type="text" placeholder="Email address" class="text-field width-half" required>
+      <input type="tel" placeholder="Phone number" class="text-field width-half float-right" required>
+      <input type="password" placeholder="Password" class="text-field width-half" required>
+      <input type="password" placeholder="Confirm password" class="text-field width-half float-right" required>
+      <input type="text" placeholder="Street" class="text-field" required>
+      <input type="text" placeholder="LGA" class="text-field width-half" required>
+      <input type="text" placeholder="State" class="text-field width-half float-right" required>
       <div class="buttons">
         <button>Create account</button>
       </div>
@@ -24,5 +29,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#signup-section{
+  .width-half{
+    width: 49%;
+  }
 
+  .float-right{
+    float: right;
+  }
+}
 </style>

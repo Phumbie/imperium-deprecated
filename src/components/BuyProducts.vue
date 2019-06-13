6dbd9626b233c7b528f31f080d8f026a1ddadfad
Kolapo Oni <onikolapo@gmail.com>
2019-06-13 12:24:52 +0100

    done product listing

--- src/components/BuyProducts.vue
+++ src/components/BuyProducts.vue
@@ -3,14 +3,16 @@
     <div class="header-text-28">Power your home with our range of products</div> 
     <div class="small-text">Find batteries, inverters, solar panels and more</div>
     <div class="products-container">
-      <div class="product-item" v-for="(n, index) in 12" :key="index" @click="goTo('/product')">
+      <div class="product-item" v-for="(product, index) in productsList" :key="index" @click="navigateTo('/product')">
         <div class="centered-content">
           <div class="image-container">
-            <img src="../assets/images/battery.jpg">
+            <img :src="product.display_image">
           </div>
-          <div class="small-text-18">10kWh Battery</div>
-          <div class="small-text-18 text-blue">3 configurations</div>
-          <div class="small-text-18 text-blue">Suggested loading</div>
+          <div class="small-text-18">{{ product.name }}</div>
+          <div class="small-text-18 text-blue">₦{{ toLocaleString(product.price) }}</div>
+          <div class="small-text-18 text-blue">{{ product.capacity }} KV</div>
+          <!-- <div class="small-text-18 text-blue">3 configurations</div> -->
+          <!-- <div class="small-text-18 text-blue">Suggested loading</div> -->
         </div>
       </div>
     </div>
@@ -18,16 +20,35 @@
 </template>
 
 <script>
+import api from "@/utils/api.js";
+
 export default {
   data () {
     return {
+      productsList: [],
     }
   },
-
+  mounted(){
+    this.fetchProducts();
+  },
   methods: {
-    goTo(page) {
+     navigateTo(page) {
       this.$router.push(page);
     },
+    toLocaleString(val) {
+      return parseFloat(val).toLocaleString();
+    },
+    fetchProducts(){
+      api
+        .getAllProducts()
+        .then(({ data }) => {
+          this.productsList = data.data.result;
+          console.log(this.productsList);
+        })
+        .catch(({ response }) => {
+      
+        });
+    }
   }
 }
 </script>

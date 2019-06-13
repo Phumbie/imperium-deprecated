702d176ddb38fa3f5e1fc588a8bf3b0ae8aba144
Kolapo Oni <onikolapo@gmail.com>
2019-06-13 12:20:31 +0100

    added product fetching endpoint & function

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 const BASE_URL = "https://altpower.herokuapp.com/v1";
 const CUSTOMER_SIGNUP_URL = `${BASE_URL}/customer`;
+const PRODUCTS_URL = `${BASE_URL}/product`;
 
 export default {
   signupCustomer: data => {
     return axios.post(CUSTOMER_SIGNUP_URL, data);
   },
+  getAllProducts: () => {
+    return axios.get(PRODUCTS_URL);
+  }
 };

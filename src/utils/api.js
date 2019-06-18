0017bf06975d149b1327843936c9937cc3f1249d
Kolapo Oni <onikolapo@gmail.com>
2019-06-18 14:28:30 +0100

    add get product by slug endpoint & function

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const CUSTOMER_SIGNUP_URL = `${BASE_URL}/customer`;
 const CUSTOMER_LOGIN_URL = `${BASE_URL}/user/login`;
 const ALL_PRODUCTS_URL = `${BASE_URL}/product`;
 const ADD_TO_CART_URL = `${BASE_URL}/cart`;
-// const SINGLE_PRODUCT_URL = `${BASE_URL}/product/search?q=`;
+const SINGLE_PRODUCT_URL = `${BASE_URL}/product`;
 
 export default {
   signupCustomer: data => {
@@ -17,8 +17,8 @@ export default {
   getAllProducts: () => {
     return axios.get(ALL_PRODUCTS_URL);
   },
-  getProductById: pid  => {
-    return axios.get(SINGLE_PRODUCT_URL+pid);
+  getProductBySlug: slug  => {
+    return axios.get(`${SINGLE_PRODUCT_URL}/${slug}`);
   },
   addProductToCart: (pid, token) => {
     let header = { 

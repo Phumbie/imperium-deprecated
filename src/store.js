e3153d1da1cd18f30dc0b048d2fa21465ad99bae
Kolapo Oni <onikolapo@gmail.com>
2019-06-30 14:21:12 +0100

    added vuex store

---
+++ src/store.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    cartCounter: 0 
+  },
+  mutations: {
+    SET_CART_COUNTER (state, payload) {
+      state.cartCounter = payload;
+    },
+    INCREMENT_CART_COUNTER (state) {
+      state.cartCounter++
+      localStorage.setItem("cartCounter", state.cartCounter);
+    },
+    DECREMENT_CART_COUNTER (state) {
+      state.cartCounter--
+      localStorage.setItem("cartCounter", state.cartCounter);
+    }
+  },
+  getters: {
+    cartCounter: state => state.cartCounter,
+  },
+  actions: { 
+    setCartCounter: ({ commit }, payLoad) => {
+      commit('SET_CART_COUNTER', payLoad);
+    },
+    incrementCartCounter: ({ commit }) => {
+      commit('INCREMENT_CART_COUNTER');
+    },
+    decrementCartCounter: ({ commit }) => {
+      commit('DECREMENT_CART_COUNTER');
+    }
+  }
+})

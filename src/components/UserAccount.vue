380ceb339c44cdd27db90adce556219bdb537792
unknown <karatemitayo@gmail.com>
2019-09-02 16:06:19 +0100

    chore: removed card details section/added help and support

--- src/components/UserAccount.vue
+++ src/components/UserAccount.vue
@@ -3,7 +3,7 @@
     <div class="header-text-28">My Account</div>
     <button class="button-logout small-text" @click="logout()">Logout</button>
     <div class="section-title">Account Details</div>
-    <span class="text-edit">Edit</span>
+    <span><router-link to="/edit-my-account" class="text-edit">Edit</router-link></span>
     <div class="details-container">
       <div class="box">
         <div class="small-header-text">Personal details</div>
@@ -15,12 +15,6 @@
         <div class="info-text">
           {{ userDetails.phone_number }}
         </div>
-      </div>
-      <div class="box">
-        <div class="small-header-text">Payment Details</div>
-        <div class="info-text">Mastercard</div>
-        <div class="info-text">5622 6799 1987 ****</div>
-        <div class="info-text">Bruno Pluto</div>
       </div>
       <div class="box">
         <div class="small-header-text">Address</div>
@@ -39,12 +33,36 @@
           <div class="small-text-18">10kWh Battery</div>
           <div class="small-gray-text">Order no: 00990987</div>
           <div class="small-gray-text">Status: Delivered</div>
-          <router-link to="my-account/order-history" tag="button" class="button-view-order">View Order</router-link>
+          <router-link to="/order-history" tag="button" class="button-view-order">View Order</router-link>
         </div>
       </div>
     </div>
     <span class="section-title">Power As A Service</span>
     <div class="paas-active-plan-container">You have no active power as a service plan</div>
+    <span class="section-title">Help & Support</span>
+    <div class="help-box-container">
+      <span class="help-box">
+        <div class="small-header-text">Shop</div>
+        <div class="info-text">Returns</div>
+        <div class="info-text">Payment</div>
+        <div class="info-text">Delivery</div>
+      </span>
+      <div class="help-box">
+        <div class="small-header-text">Power As A Service</div>
+        <div class="info-text">Calculator</div>
+        <div class="info-text">Plans</div>
+        <div class="info-text">Outright Purchase</div>
+        <div class="info-text">Lease to Own</div>
+      </div>
+      <div class="help-box">
+        <div class="small-header-text">AltPay</div>
+        <div class="info-text">Terms & Conditions</div>
+      </div>
+      <span class="help-box">
+        <p class="small-header-text">Can't find what you're looking for?</p>
+        <router-link to="#" tag="button" class="button-send-mail">Send us an email</router-link>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -101,6 +119,8 @@ export default {
 
   .text-edit{
     font-size: 15px;
+    color: #000000;
+    text-decoration: none;
     cursor: pointer;
   }
 
@@ -127,6 +147,7 @@ export default {
 
   .paas-active-plan-container{
     border: solid 1px #000000;
+    margin: 2% 0% 2% 0%;
     padding-top: 114px;
     padding-bottom: 114px;
     text-align: center;
@@ -138,6 +159,70 @@ export default {
     // margin: 0 auto;
     background: transparent;
     border: none;
+  }
+  .box{
+    width: 50%;
+  }
+  .details-container :last-child{
+    border-right: none;
+  }
+  .help-box-container{
+    margin-top: 29px;
+    width: 100%;
+    border: solid 1px #000000;
+    display: flex;
+
+    .border-right-none{
+      border-right: none !important;
+    }
+
+    .help-box{
+      width: 20%;
+      padding-top: 30px;
+      padding-left: 20px;
+      padding-bottom: 20px;
+      padding-right: 0px;
+
+      .small-header-text{
+        font-size: 18px;
+        font-weight: 500;
+        margin-bottom: 25px;
+      }
+
+      .info-text{
+        font-size: 12px;
+        margin-bottom: 20px;
+        font-weight: 500;
+        color: #414b55;
+      }
+    }
+  }
+  .help-box-container span:last-child{
+    width: 60%;
+    border-right: none;
+    border-left: solid 1px black;
+    text-align: center;
+  }
+  .help-box-container span:first-child{
+    width: 13%;
+  }
+  .button-send-mail{
+    width: 50%;
+    // margin-top: 25px;
+    font-size: 14px;
+    background: none;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    transition: 0.5s ease-in-out;
+    outline: none;
+  }
+  .button-send-mail:hover{
+    background: black;
+    color: white;
+    transition: 0.5s ease-in-out;
+  }
+  .help-box p:first-child{
+    margin-top: 7%;
   }
 }
 </style>

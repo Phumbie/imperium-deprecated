dc6feeace093961f4130cca518622865030520a3
unknown <karatemitayo@gmail.com>
2019-09-08 04:59:23 +0100

    styled  user account page

--- src/components/UserAccount.vue
+++ src/components/UserAccount.vue
@@ -2,75 +2,52 @@
   <div id="user-account">
     <div class="header-text-28">My Account</div>
     <button class="button-logout small-text" @click="logout()">Logout</button>
-    <div class="section-title">Account Details</div>
-    <span><router-link to="/edit-my-account" class="text-edit">Edit</router-link></span>
-    <div class="details-container">
-      <div class="box">
-        <div class="small-header-text">Personal details</div>
-        <div class="info-text">
-          {{ userFullName }}
-        </div>
-        <div class="info-text">{{ userDetails.email }}</div>
-        <div class="info-text">Non binary</div>
-        <div class="info-text">
-          {{ userDetails.phone_number }}
-        </div>
-      </div>
-      <div class="box">
-        <div class="small-header-text">Address</div>
-        <div class="info-text">Sterling Towers, 20 Marina Road,</div>
-        <div class="info-text">Lagos-Island,</div>
-        <div class="info-text">100201</div>
-      </div>
-    </div>
-    <span class="section-title">
-      Order History
-      <span class="view-all-title">
-        <router-link to="/order-history" class="text-edit">
-          View All
-        </router-link>
-      </span>
-    </span>
-    <div class="products-container">
-      <div class="product-item" v-for="(n, index) in 4" :key="index">
-        <div class="centered-content">
-          <div class="image-container">
-            <img src="../assets/images/battery.jpg">
+    <div id="account-section">
+      <div class="header-text-28">Account Details</div>
+      <span><router-link to="/edit-my-account" class="header-text-28 text-edit">Edit</router-link></span>
+      <div class="details-container">
+        <div class="box">
+          <div class="small-header-text">Personal details</div>
+          <div class="info-text info-text-name">
+            {{ userFullName }}
           </div>
-          <div class="small-text-18">10kWh Battery</div>
-          <div class="small-gray-text">Order no: 00990987</div>
-          <div class="small-gray-text">Status: Delivered</div>
-          <router-link to="/order-history" tag="button" class="button-view-order">View Order</router-link>
+          <div class="info-text">{{ userDetails.email }}</div>
+          <div class="info-text">Non binary</div>
+          <div class="info-text">
+            {{ userDetails.phone_number }}
+          </div>
+        </div>
+        <div class="box">
+          <div class="small-header-text">Address</div>
+          <div class="info-text">Sterling Towers, 20 Marina Road,</div>
+          <div class="info-text">Lagos-Island,</div>
+          <div class="info-text">100201</div>
         </div>
       </div>
+      <div class="header-text-28">
+        Order History
+        <span>
+          <router-link to="/order-history" class="header-text-28 text-edit btn-view-all">
+            View All
+          </router-link>
+        </span>
+      </div>
+      <div class="products-container">
+        <div class="product-item" v-for="(n, index) in 4" :key="index">
+          <div class="centered-content">
+            <div class="image-container">
+              <img src="../assets/images/battery.jpg">
+            </div>
+            <div class="small-text-18">10kWh Battery</div>
+            <div class="small-gray-text">Order no: 00990987</div>
+            <div class="small-gray-text">Status: Delivered</div>
+            <router-link to="/order-history" tag="button" class="button-view-order">View Order</router-link>
+          </div>
+        </div>
+      </div>
+      <span class="header-text-28">Power As A Service</span>
+      <div class="paas-active-plan-container">You have no active power as a service plan</div>
     </div>
-    <span class="section-title">Power As A Service</span>
-    <div class="paas-active-plan-container">You have no active power as a service plan</div>
-
-    <!-- <span class="section-title">Help & Support</span>
-    <div class="help-box-container">
-      <span class="help-box">
-        <div class="small-header-text">Shop</div>
-        <div class="info-text">Returns</div>
-        <div class="info-text">Payment</div>
-        <div class="info-text">Delivery</div>
-      </span>
-      <div class="help-box">
-        <div class="small-header-text">Power As A Service</div>
-        <div class="info-text">Calculator</div>
-        <div class="info-text">Plans</div>
-        <div class="info-text">Outright Purchase</div>
-        <div class="info-text">Lease to Own</div>
-      </div>
-      <div class="help-box">
-        <div class="small-header-text">AltPay</div>
-        <div class="info-text">Terms & Conditions</div>
-      </div>
-      <span class="help-box">
-        <p class="small-header-text">Can't find what you're looking for?</p>
-        <router-link to="#" tag="button" class="button-send-mail">Send us an email</router-link>
-      </span>
-    </div> -->
   </div>
 </template>
 
@@ -110,79 +87,20 @@ export default {
     width: auto !important;
   }
 
-  .small-text-18{
-    margin-bottom: 18px;
-  }
-
-  .small-gray-text{
-    font-size: 15px;
-    margin-top: 12px;
-  }
-
-  .section-title{
-    margin-top: 60px;
-    font-size: 19px;
-    margin-bottom: 18px;
-  }
-  .view-all-title{
-    float: right;
-  }
-
-  .text-edit{
-    font-size: 15px;
-    color: #000000;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
-  .button-view-order{
-    width: 100%;
-    margin-top: 25px;
-    font-size: 14px;
-    background: none;
-    padding-top: 12px;
-    padding-bottom: 12px;
-    transition: 0.5s ease-in-out;
-    outline: none;
-  }
-
-  .button-view-order:hover{
-    background: black;
-    color: white;
-    transition: 0.5s ease-in-out;
-  }
 
   .products-container{
-    margin-bottom: 50px;
+    margin-bottom: 10%;
   }
 
-  .details-container{
-    margin-bottom: 50px;
-    width: 99.9%;
-  }
 
-  .paas-active-plan-container{
-    border: solid 1px #000000;
-    margin: 2% 0% 2% 0%;
-    padding-top: 114px;
-    padding-bottom: 114px;
-    text-align: center;
-    color: #1d1d1d;
-  }
 
   .button-logout{
     width: 100px;
-    // margin: 0 auto;
+    margin: 16px auto;
     background: transparent;
     border: none;
   }
 
-  .box{
-    width: 50%;
-  }
-  .details-container :last-child{
-    border-right: none;
-  }
   .help-box-container{
     margin-top: 29px;
     width: 100%;
@@ -198,31 +116,21 @@ export default {
       padding-top: 30px;
       padding-left: 20px;
       padding-bottom: 20px;
-      padding-right: 0px;
-
-      .small-header-text{
-        font-size: 18px;
-        font-weight: 500;
-        margin-bottom: 25px;
-      }
-
-      .info-text{
-        font-size: 12px;
-        margin-bottom: 20px;
-        font-weight: 500;
-        color: #414b55;
-      }
+      padding-right: 0px;  
     }
   }
+
   .help-box-container span:last-child{
     width: 60%;
     border-right: none;
     border-left: solid 1px black;
     text-align: center;
   }
+
   .help-box-container span:first-child{
     width: 13%;
   }
+
   .button-send-mail{
     width: 50%;
     // margin-top: 25px;
@@ -233,6 +141,7 @@ export default {
     transition: 0.5s ease-in-out;
     outline: none;
   }
+
   .button-send-mail:hover{
     background: black;
     color: white;
@@ -242,4 +151,99 @@ export default {
     margin-top: 7%;
   }
 }
+
+#account-section{
+  .header-text-28{
+    // width: 178px;
+    text-align: left;
+    height: 24px;
+    font-size: 24px;
+    font-weight: normal;
+    margin-top: 60px;
+    margin-bottom: 18px;
+  }
+  .text-edit{
+    // width: 30px;
+    height: 16px;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+  } 
+  .details-container{
+    margin-bottom: 10%;
+    width: 99.9%;
+  }
+  .box{
+    width: 50%;
+  }
+  .details-container :last-child{
+    border-right: none;
+  }
+  .small-header-text{
+    // width: 168px;
+    height: 22px;
+    font-size: 22px;
+  }
+  .info-text-name{
+    font-weight: 500;
+  }
+  .btn-view-all{
+    float: right;
+    margin: 0 0;
+  }
+  .small-gray-text{
+    // width: 143px;
+    height: 14px;
+    font-family: Graphik;
+    font-size: 14px;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: 0.34px;
+    color: #414b55;
+    margin-top: 12px;
+  }
+  .button-view-order{
+    // width: 73px;
+    // height: 14px;
+    font-family: Graphik;
+    font-size: 14px;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: center;
+    width: 100%;
+    margin-top: 25px;
+    background: none;
+    padding: 12px 0;
+    transition: 0.5s ease-in-out;
+    outline: none;
+  }
+
+  .button-view-order:hover{
+    background: #1d1d1d;
+    color: white;
+    transition: 0.5s ease-in-out;
+  }
+  .paas-active-plan-container{
+    // width: 459px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.5;
+    letter-spacing: normal;
+    height: 27px;
+    font-family: Graphik;
+    font-size: 18px;
+    border: solid 1px #000000;
+    margin: 4% 0;
+    padding: 114px 0;
+    text-align: center;
+    color: #1d1d1d;
+  }
+}
+  
 </style>

7ed406ee15a030ac924252e0450c82c6f0b4397b
unknown <karatemitayo@gmail.com>
2019-09-09 16:07:54 +0100

    chore: made changes

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -98,7 +98,7 @@ export default {
     return {
       fetchedCart: false,
       customerCart: {},
-      contentLoaderText: "Fetching cart..."
+      contentLoaderText: "Nothing to show"
     }
   },
   mounted(){
@@ -350,6 +350,11 @@ export default {
     height: 50px;
     animation: spin 1s linear infinite;
     margin: 0% auto;
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
   }
 }
 </style>

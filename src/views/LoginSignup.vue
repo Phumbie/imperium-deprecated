3d4ebc86cc8ea4ccfd88ce97024d841c0bad489c
unknown <karatemitayo@gmail.com>
2019-09-09 17:13:27 +0100

    created login signup component

---
+++ src/views/LoginSignup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="login-signup">
+    <top-nav/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TopNav from '@/components/TopNav'
+
+export default {
+  components: {
+    TopNav,
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#login-signup{
+  width: 80%;
+  margin: 0 auto;
+}
+</style>

7caf450a84eb4aa19c9a5d0487af56f18766a5c9
unknown <karatemitayo@gmail.com>
2019-09-27 05:54:36 +0100

    implemented local cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -56,6 +56,61 @@
         </div>
       </div>
     </div>
+        <div class="cart-list" v-else-if ="fetchedLocalStorage">
+      <div class="header-section">
+        <div class="text-product-details">Product details</div>
+        <div class="text-quantity">Quantity</div>
+        <div class="text-amount">Total amount</div>
+      </div>
+      <div 
+        class="cart-item" 
+        v-for="(n, index) in clientArr.length" 
+        :key="index"
+      >
+        <div class="product-img-name-section">
+          <div class="image-container">
+            <img :src="clientArr[index].display_image">
+          </div>
+          <div class="product-name-price-section">
+            <div class="product-name">
+              {{ clientArr[index].name }}
+            </div>
+            <div class="price">
+              ₦ {{ (clientArr[index].price/100).toLocaleString() }}
+            </div>
+            <button 
+              class="button-remove" 
+              @click="removeProductFromCart(clientArr[index].id)"
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+        <div class="quantity-section">
+          <button 
+            class="button-minus"
+            @click="decreaseProductQuantity(clientArr[index].id)"
+          >
+            -
+          </button>
+          <span class="quantity">{{ clientArr[index].quantity }}</span>
+          <button
+            class="button-plus"
+            @click="increaseProductQuantity(clientArr[index].id)"
+          >
+            +
+          </button>
+        </div>
+        <div class="amount-section">
+          <div class="amount">
+            ₦ {{ 
+              (clientArr[index].price * clientArr[index]
+              .quantity/100).toLocaleString() 
+            }}
+          </div>
+        </div>
+      </div>
+    </div>
     <content-loader v-else>
       <div v-if="show" class="loader"></div>
       <span v-else>{{ contentLoaderText }}</span>
@@ -68,6 +123,28 @@
         <span>Subtotal</span>
         <span>
           ₦ {{ (subtotal/100).toLocaleString() }}
+        </span>
+      </div>
+      <div class="text">
+        Delivery and taxes are calculated at checkout
+      </div>
+      <div class="buttons">
+        <button @click="navigateTo('/')">
+          Continue shopping
+        </button>
+        <button class="bg-black" @click="checkout()">
+          Check out
+        </button>
+      </div>
+    </div>
+    <div 
+      class="shop-checkout-buttons-section"
+      v-else-if ="fetchedLocalStorage"
+    >
+      <div class="subtotal-section">
+        <span>Subtotal</span>
+        <span>
+          ₦ {{ (subtotalArr/100).toLocaleString() }}
         </span>
       </div>
       <div class="text">
@@ -97,6 +174,11 @@ export default {
   data () {
     return {
       fetchedCart: false,
+      fetchedLocalStorage: false,
+      productsList: [],
+      clientArr: [],
+      localCart: [],
+      subtotalArr: [],
       customerCart: {},
       contentLoaderText: "",
       show: true
@@ -118,36 +200,91 @@ export default {
       this.$router.push(page);
     },
     getCart() {
-      api
-        .getCart()
+      if(!localStorage.getItem("user_details")) {
+        api
+        .getAllProducts()
         .then(({ data }) => {
-          if(data.status == "success"){
-            this.customerCart = data.data;
-            this.checkIfCartIsEmpty();
-          }
+          this.productsList = data.data.result;
+          JSON.parse(localStorage.getItem("product_id")).map(item => {
+            this.productsList.map(product => {
+              if(product.id === item.id) {
+                product.local_id = item.id
+                product.quantity = item.quantity
+                product.subtotal = item.subtotal
+                this.clientArr.push(product)
+              }
+            })
+          })
+          JSON.parse(localStorage.getItem("product_id")).map((item, index) => {
+            this.subtotalArr.push(item.subtotal)
+          })
+          localStorage.setItem("subtotal_arr", JSON.stringify(this.subtotalArr))
+          localStorage.setItem("local_cart", JSON.stringify(this.clientArr))
+          this.subtotalArr = this.subtotalArr.reduce(function(acc, value){
+            return acc + value}, 0) 
+          localStorage.setItem("subtotal", JSON.stringify(this.subtotalArr))
+          this.checkIfLocalStorageIsEmpty()
         })
         .catch(({ response }) => {
-          let errorMessage = "Unable to fetch cart items :("
-          if(response){
-            errorMessage = this.response.message;
-          }
-          alert(errorMessage);
         });
+        return
+      }
+        api
+          .getCart()
+          .then(({ data }) => {
+            if(data.status == "success"){
+              this.customerCart = data.data;
+              this.checkIfCartIsEmpty();
+              // console.log(this.customerCart);
+            }
+          })
+          .catch(({ response }) => {
+            let errorMessage = "Unable to fetch cart items :("
+            if(response){
+              errorMessage = this.response.message;
+            }
+            alert(errorMessage);
+          });
     },
     removeProductFromCart(productId) {
-      api
-        .removeFromCart(productId)
-        .then(({ data }) => {
-          if(data.status == "success"){
-            this.customerCart = data.data;
-            this.checkIfCartIsEmpty();
-            alert("Successfully removed product from cart");
+      if(!localStorage.getItem("user_details")) {
+        let local_items = JSON.parse(localStorage.getItem("product_id"))
+        local_items.map((item, index) => {
+          if(item.id === productId){
+            item.quantity = 0
+            this.subtotalArr -= item.subtotal
+            local_items.splice(index, 1)
           }
         })
-        .catch(({ response }) => {
-          // alert("An error occurred while removing product");
-          alert(response.data.message);
-        });
+        localStorage.setItem("product_id", JSON.stringify(local_items))
+        this.clientArr = []
+          JSON.parse(localStorage.getItem("product_id")).map(item => {
+            this.productsList.map(product => {
+              if(product.id === item.id) {
+                product.local_id = item.id
+                product.quantity = item.quantity
+                this.clientArr.push(product)
+              }
+            })
+          })
+        localStorage.setItem("local_cart", JSON.stringify(this.clientArr))
+        this.checkIfLocalStorageIsEmpty()
+        alert("Successfully removed product from cart");
+        return
+      }
+        api
+          .removeFromCart(productId)
+          .then(({ data }) => {
+            if(data.status == "success"){
+              this.customerCart = data.data;
+              this.checkIfCartIsEmpty();
+              alert("Successfully removed product from cart");
+            }
+          })
+          .catch(({ response }) => {
+            // alert("An error occurred while removing product");
+            alert(response.data.message);
+          });
     },
     checkIfCartIsEmpty() {
       this.fetchedCart = false;
@@ -160,7 +297,37 @@ export default {
 
       this.fetchedCart = true;
     },
+    checkIfLocalStorageIsEmpty() {
+      this.fetchedLocalStorage = false;
+
+      if(this.clientArr.length === 0){
+        this.show = false;
+        this.contentLoaderText = "Nothing to show";
+        return;
+      }
+
+      this.fetchedLocalStorage = true;
+    },
     decreaseProductQuantity(productId) {
+      if(!localStorage.getItem("user_details")) {
+        let localCart = JSON.parse(localStorage.getItem("local_cart"))
+        let localQuantity = JSON.parse(localStorage.getItem("product_id"))
+        localCart.map(item => {
+          localQuantity.map(items => {
+            if(item.id === productId && items.id === productId && item.quantity !== 0){
+              item.quantity = item.quantity -= 1
+              items.quantity = items.quantity -= 1
+              items.subtotal -= item.price
+              this.subtotalArr -= item.price
+              localStorage.setItem("local_cart", JSON.stringify(localCart))
+              localStorage.setItem("product_id", JSON.stringify(localQuantity))
+              this.clientArr = JSON.parse(localStorage.getItem("local_cart"))
+            }
+          })
+        })
+        this.$store.dispatch('decrementCartCounter');
+        return
+      }
       api
         .decreaseProductQuantityInCart(productId)
         .then(({ data }) => {
@@ -175,6 +342,25 @@ export default {
         });
     },
     increaseProductQuantity(productId) {
+      if(!localStorage.getItem("user_details")) {
+        let localCart = JSON.parse(localStorage.getItem("local_cart"))
+        let localQuantity = JSON.parse(localStorage.getItem("product_id"))
+        localCart.map(item => {
+          localQuantity.map(items => {
+            if(item.id === productId && items.id === productId){
+              item.quantity = item.quantity += 1
+              items.quantity = items.quantity += 1
+              items.subtotal += item.price
+              this.subtotalArr += item.price
+              localStorage.setItem("local_cart", JSON.stringify(localCart))
+              localStorage.setItem("product_id", JSON.stringify(localQuantity))
+              this.clientArr = JSON.parse(localStorage.getItem("local_cart"))
+            }
+          })
+        })
+        this.$store.dispatch('incrementCartCounter');
+        return
+      }
       api
         .addProductToCart(productId)
         .then(({ data }) => {
@@ -188,6 +374,11 @@ export default {
         });
     },
     checkout() {
+      if (!localStorage.getItem("user_details")) {
+        alert("You have to login or signup to check out 🙃");
+        this.navigateTo("/login");
+        return;
+      }
       api
         .cartCheckout()
         .then(({ data }) => {

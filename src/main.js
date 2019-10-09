3051e9a4013ed1208fea0b2d8780ef64bb7c042a
unknown <karatemitayo@gmail.com>
2019-10-09 15:22:49 +0100

    imported sweet alert

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VModal from 'vue-js-modal'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import { store }  from './store'
 
 Vue.use(VModal)
 
+Vue.use(VueSweetalert2);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

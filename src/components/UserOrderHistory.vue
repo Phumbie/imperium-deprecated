db7a6b7a60719a9eefaa63053aba94d3b91cc50b
lavikara <karatemitayo@gmail.com>
2019-10-15 10:43:53 +0100

    fetched order history and rendered it on page

--- src/components/UserOrderHistory.vue
+++ src/components/UserOrderHistory.vue
@@ -1,40 +1,41 @@
 <template>
-  <div id="user-order-history">
+  <div id="user-order-history" v-if="loading">
     <div class="header-text-28">Order Details</div>
     <!-- <div class="small-text">Thank you for your order. Check your details below.</div> -->
     <div class="details-container">
       <div class="box">
         <div class="small-header-text">Order Sent</div>
-        <div class="info-text">Bruno Pluto</div>
-        <div class="info-text">earthyjupiter@moon.com</div>
-        <div class="info-text">Gender binary</div>
-        <div class="info-text">+2349071657890</div>
+        <div class="info-text capitalize">{{ this.userFullName }}</div>
+        <div class="info-text">{{ this.userDetails.user.email }}</div>
+        <div class="info-text">{{ this.date[0] }}</div>
+        <div class="info-text capitalize">{{ `${this.userDetails.address.street}, ${this.userDetails.address.lga}, ${this.userDetails.address.state}.` }}</div>
       </div>
       <div class="box border-right-none">
         <div class="small-header-text">Order Total</div>
         <div class="amount-row">
           <span class="info-text">Sub-Total:</span>
-          <span class="info-text float-right">NGN700,000.00</span>
+          <span class="info-text float-right">₦ {{ (this.placedOrder.sub_total/100).toLocaleString() }}</span>
         </div>
         <div class="amount-row">
           <span class="info-text">Delivery:</span>
-          <span class="info-text float-right">NGN2,000.00</span>
+          <span class="info-text float-right">₦ {{ (this.placedOrder.delivery_cost/100).toLocaleString() }}</span>
         </div>
         <div class="amount-row">
           <span class="info-text">Total:</span>
-          <span class="info-text float-right">NGN702,000.00</span>
+          <span class="info-text float-right">₦ {{ (this.placedOrder.total_price/100).toLocaleString() }}</span>
         </div>
       </div>
     </div>
     <span class="section-title">Items</span>
     <div class="products-container">
-      <div class="product-item" v-for="(n, index) in 4" :key="index">
+      <div class="product-item" v-for="(item, index) in orderItems" :key="index">
         <div class="centered-content">
           <div class="image-container">
-            <img src="../assets/images/battery.jpg">
+            <img :src="`${ item.display_image }`">
           </div>
-          <div class="small-text-18">10kWh Battery</div>
-          <div class="small-gray-text">NGN 200,000.00</div>
+          <div class="small-text-18">{{ item.name }}</div>
+          <div class="small-gray-text"> {{ `Price: ₦ ${(item.price/100).toLocaleString()}` }}</div>
+          <div class="small-gray-text">{{ `Quantity: ${item.quantity}` }}</div>
         </div>
       </div>
     </div>
@@ -45,7 +46,25 @@
 export default {
   data () {
     return {
+      loading: false,
+      userDetails: "",
+      userFullName: "",
+      placedOrder: [],
+      orderItems: [],
+      date: ""
     }
+  },
+  mounted(){
+    if(!localStorage.getItem("user_details")) {
+      this.navigateTo("login");
+      return;
+    }
+    this.placedOrder = JSON.parse(localStorage.getItem('placed_order'));
+    this.orderItems = JSON.parse(localStorage.getItem('placed_items'));
+    this.userDetails = JSON.parse(localStorage.getItem('user_details'));
+    this.date = (this.placedOrder.paid_at).split("T")
+    this.userFullName = `${this.userDetails.first_name} ${this.userDetails.last_name}`
+    this.loading = true
   },
 
   methods: {

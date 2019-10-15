4cad0a85b911d98cb43a28ef52ff2f563a85271c
lavikara <karatemitayo@gmail.com>
2019-10-15 10:43:11 +0100

    fetched order history and rendered it on page

--- src/components/UserAccount.vue
+++ src/components/UserAccount.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="user-account" v-if="userDetails">
+  <div id="user-account" v-if="loading">
     <div class="header-text-28">My Account</div>
     <button class="button-logout small-text" @click="logout()">Logout</button>
     <div class="section-title">Account Details</div>
@@ -31,36 +31,54 @@
       </div>
     </div>
     <span class="section-title">Order History</span>
-    <div class="products-container">
-      <div class="product-item" v-for="(n, index) in 4" :key="index">
+    <div class="products-container" v-if="hasHistory">
+      <div class="product-item" v-for="(order, index) in orderHistory" :key="index">
         <div class="centered-content">
           <div class="image-container">
-            <img src="../assets/images/battery.jpg">
+            <img :src="`${order.items[0].display_image}`">
           </div>
-          <div class="small-text-18">10kWh Battery</div>
-          <div class="small-gray-text">Order no: 00990987</div>
-          <div class="small-gray-text">Status: Delivered</div>
-          <router-link 
+          <div class="small-text-18">{{ `Date: ${(order.created_at).split("T")[0]}` }}</div>
+          <div class="small-gray-text">{{ `Order ID: ${order.transaction_id}` }}</div>
+          <div class="small-gray-text">{{ `Status: ${ order.status }`}}</div>
+          <button 
+            id="btn"
+            :disabled="disabledBtn"
             to="my-account/order-history" 
             tag="button" 
             class="button-view-order"
+            @click="getOrderHistoryById(order.id)"
           >
             View Order
-          </router-link>
+          </button>
         </div>
       </div>
     </div>
+    <content-loader v-else>
+      <span>{{ contentLoaderText }}</span>
+    </content-loader>
     <span class="section-title">Power As A Service</span>
     <div class="paas-active-plan-container">You have no active power as a service plan</div>
   </div>
 </template>
 
 <script>
+import api from "@/utils/api.js";
+import contentLoader from "@/components/ContentLoader"
+
 export default {
+  name: 'UserAccount',
+  components: {
+    contentLoader
+  },
   data () {
     return {
+      loading: false,
+      hasHistory: false,
+      disabledBtn: false,
       userDetails: "",
       userFullName: "",
+      orderHistory: [],
+      contentLoaderText: "",
     }
   },
   mounted(){
@@ -72,6 +90,20 @@ export default {
     this.userDetails = JSON.parse(localStorage.getItem('user_details'));
     // console.log(this.userDetails)
     this.userFullName = `${this.userDetails.first_name} ${this.userDetails.last_name}`
+
+    api
+      .getCustomerOrder()
+      .then(({ data }) => {
+        if(data.status == "success"){
+          this.orderHistory = data.data.result
+          this.orderHistory.length === 0 ? this.hasHistory = false : this.hasHistory = true
+          this.contentLoaderText = "Nothing to show"
+          this.loading = true
+        }
+      })
+      .catch(({ response }) => {
+        alert(response.data.message);
+      });
   },
   methods: {
     logout(){
@@ -81,6 +113,22 @@ export default {
     navigateTo(page) {
       this.$router.push(page); 
     },
+    getOrderHistoryById(orderId){
+      //disable button when clicked
+      this.disabledBtn = true;
+      api
+        .getCustomerOrderById(orderId)
+        .then(({ data }) => {
+          if(data.status == "success"){
+            localStorage.setItem("placed_order", JSON.stringify(data.data))
+            localStorage.setItem("placed_items", JSON.stringify(data.data.items))
+          }
+          this.navigateTo('my-account/order-history');
+        })
+        .catch(({ response }) => {
+          alert(response.data.message)
+        })
+    }
   }
 }
 </script>
@@ -90,10 +138,6 @@ export default {
   a{
     text-decoration: none;
     width: auto !important;
-  }
-
-  .capitalize {
-    text-transform: capitalize;
   }
 
   .small-text-18{

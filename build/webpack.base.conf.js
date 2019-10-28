1ff00ae9461b8718f3f04a049e87180ecd58a4b1
lavikara <karatemitayo@gmail.com>
2019-10-28 14:07:11 +0100

    added '*' to webpack extensions

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,11 +23,11 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-    }
+    },
+    extensions: ['*', '.js', '.vue', '.json']
   },
   module: {
     rules: [

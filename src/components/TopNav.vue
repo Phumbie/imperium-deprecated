611fa0b75183185452e2e8003e65adb20ed38aaf
lavikara <karatemitayo@gmail.com>
2020-03-04 08:38:38 +0100

    style: styled topnav component

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,17 +2,11 @@
   <div id="nav-container">
     <div class="top-section">
       <router-link to="/my-account" class="nav-item">Account</router-link>
-      <router-link to="/my-account" class="mobile-nav-item border-right"
-        >Account</router-link
-      >
-      <div class="product-title">Imperium</div>
+      <router-link to="/" class="product-title">Imperium</router-link>
       <router-link to="/cart" class="nav-item"
         >Shopping cart ({{
           cartCounter === 0 ? "..." : cartCounter
         }})</router-link
-      >
-      <router-link to="/cart" class="mobile-nav-item border-left"
-        >cart ({{ cartCounter === 0 ? "..." : cartCounter }})</router-link
       >
     </div>
     <div class="bottom-section">
@@ -22,24 +16,10 @@
         class="nav-item1"
         >Energy Calculator</a
       >
-      <a
-        href="https://calculator.imperiumng.com/"
-        target="_blank"
-        class="mobile-nav-item1"
-        >Energy Calc</a
-      >
       <router-link to="#" class="nav-item2">Buy Power</router-link>
-      <router-link to="#" class="mobile-nav-item2">Buy Power</router-link>
-      <router-link to="/" class="nav-item3">Buy Products</router-link>
-      <router-link to="/" class="mobile-nav-item3">Buy Products</router-link>
+      <router-link to="/products" class="nav-item3">Buy Products</router-link>
       <a
         class="nav-item4"
-        href="https://www.imperiumng.com/blog"
-        target="_blank"
-        >Blog</a
-      >
-      <a
-        class="mobile-nav-item4"
         href="https://www.imperiumng.com/blog"
         target="_blank"
         >Blog</a
@@ -115,7 +95,6 @@ export default {
 #nav-container {
   border: solid 1px #000000;
   width: 99.9%;
-  // border-right: none;
   position: sticky;
   position: -webkit-sticky;
   margin-top: 38px;
@@ -139,11 +118,12 @@ export default {
     border-right: solid 1px #000000;
     border-left: solid 1px #000000;
     border-bottom: solid 1px #000000;
-    width: 49.9%;
-    font-size: 32px;
-    padding: 15px 0;
+    width: 50%;
+    font-size: 2rem;
+    text-decoration: none;
+    padding: 0.6rem 0;
     text-align: center;
-    color: black;
+    color: #1d1d1d;
   }
 
   .nav-item {
@@ -151,6 +131,11 @@ export default {
     align-items: center;
     justify-content: center;
     border-bottom: solid 1px #000000;
+    width: 49.9%;
+    font-size: 32px;
+    padding: 15px 0;
+    text-align: center;
+    color: black;
   }
 
   .nav-item,
@@ -159,12 +144,12 @@ export default {
   .nav-item3,
   .nav-item4 {
     flex: 1;
+    font-size: 1.1rem;
     background: white;
-    padding-top: 25px;
-    padding-bottom: 25px;
+    padding: 1.2rem 0;
     text-align: center;
     text-decoration: none;
-    color: black;
+    color: #1d1d1d;
     transition: 0.5s;
     cursor: pointer;
   }
@@ -179,101 +164,10 @@ export default {
     transition: 0.5s;
   }
 
-  .nav-item1 {
-    border-right: solid 1px #000000;
-  }
-
-  .nav-item2 {
-    border-right: solid 1px #000000;
-  }
-
+  .nav-item1,
+  .nav-item2,
   .nav-item3 {
     border-right: solid 1px #000000;
-  }
-
-  .mobile-nav-item,
-  .mobile-nav-item1,
-  .mobile-nav-item2,
-  .mobile-nav-item3,
-  .mobile-nav-item4 {
-    display: none;
-    // pointer-events: none;
-  }
-}
-
-//Mobile
-@media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
-  #nav-container {
-    width: 99.7%;
-
-    .product-title {
-      width: calc(50% - 0.5px);
-      font-size: 28px;
-      border: none;
-      border-bottom: solid 1px #000000;
-    }
-
-    .mobile-nav-item {
-      border-bottom: solid 1px #000000;
-    }
-
-    .mobile-nav-item,
-    .mobile-nav-item1,
-    .mobile-nav-item2,
-    .mobile-nav-item3,
-    .mobile-nav-item4 {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex: 1;
-      background: white;
-      padding-top: 25px;
-      padding-bottom: 25px;
-      font-size: 12px;
-      text-decoration: none;
-      color: black;
-      transition: 0.5s;
-      cursor: pointer;
-    }
-
-    .border-right {
-      border-right: solid 1px #000000;
-    }
-
-    .border-left {
-      border-left: solid 1px #000000;
-    }
-
-    .mobile-nav-item4 {
-      border-left: solid 1px #000000;
-    }
-
-    .mobile-nav-item2 {
-      border-left: solid 1px #000000;
-    }
-
-    .mobile-nav-item3 {
-      border-left: solid 1px #000000;
-    }
-
-    .mobile-nav-item:hover,
-    .mobile-nav-item1:hover,
-    .mobile-nav-item2:hover,
-    .mobile-nav-item3:hover,
-    .mobile-nav-item4:hover {
-      background: #000000;
-      color: white;
-      transition: 0.5s;
-    }
-
-    .nav-item,
-    .nav-item1,
-    .nav-item2,
-    .nav-item3,
-    .nav-item4 {
-      display: none;
-      // pointer-events: none;
-    }
   }
 }
 </style>

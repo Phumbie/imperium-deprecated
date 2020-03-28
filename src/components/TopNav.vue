d6b5ae1a885aa33691489131068df7fbcfb6d3a5
lavikara <karatemitayo@gmail.com>
2020-03-28 12:14:22 +0100

    feat: created and styled hamburger menu

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,14 +1,50 @@
 <template>
   <div id="nav-container">
     <section class="top-section">
-      <router-link to="/my-account" class="nav-item border-left"
+      <router-link to="/my-account" class="nav-item border-left desktop-view"
         >Account</router-link
       >
+      <div class="mobile-nav-item breadcrumb border-left mobile-view">
+        <input type="checkbox" class="toggler" />
+        <div class="hamburger"><div></div></div>
+        <div class="menu">
+          <!-- <div>
+            <ul>
+              <li>
+                <a href="https://www.imperiumng.com/about">About</a>
+              </li>
+              <li>
+                <a href="/">
+                  Calculator
+                </a>
+              </li>
+              <li>
+                <a :href="shopURL">
+                  Shop
+                </a>
+              </li>
+              <li>
+                <a href="https://www.imperiumng.com/faqs">
+                  Faqs
+                </a>
+              </li>
+              <li>
+                <a href="https://www.imperiumng.com/blog">
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div> -->
+        </div>
+      </div>
       <router-link to="/" class="product-title">Imperium</router-link>
-      <router-link to="/cart" class="nav-item border-right"
+      <router-link to="/cart" class="nav-item border-right desktop-view"
         >Shopping cart ({{
           cartCounter === 0 ? "0" : cartCounter
         }})</router-link
+      >
+      <router-link to="/cart" class="mobile-nav-item border-right mobile-view"
+        >Cart ({{ cartCounter === 0 ? "0" : cartCounter }})</router-link
       >
     </section>
     <section class="bottom-section">
@@ -124,7 +160,7 @@ export default {
 
 <style lang="scss" scoped>
 #nav-container {
-  width: 100%;
+  width: calc(100% + 1px);
   border-right: none;
   border-left: none;
   border-bottom: none;
@@ -138,12 +174,142 @@ export default {
     display: grid;
     grid-template-columns: 1fr 2fr 1fr;
     border-top: solid 0.0625rem #000000;
+
+    .breadcrumb {
+      position: relative;
+      font-size: 14px;
+      right: 0;
+      top: 0;
+      z-index: 100;
+
+      .toggler {
+        position: absolute;
+        right: 2rem;
+        top: 1.2rem;
+        z-index: 300;
+        cursor: pointer;
+        width: 1.8rem;
+        height: 3rem;
+        opacity: 0;
+      }
+
+      .hamburger {
+        position: absolute;
+        right: 2rem;
+        top: 1.2rem;
+        z-index: 200;
+        cursor: pointer;
+        width: 2rem;
+        height: 3.2rem;
+        padding: 1px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      // hamburger line
+      .hamburger > div {
+        position: relative;
+        width: 100%;
+        height: 0.2rem;
+        background-color: black;
+        opacity: 0.6;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: all 0.2s ease;
+      }
+      .hamburger > div:before,
+      .hamburger > div:after {
+        content: "";
+        position: absolute;
+        top: -0.6rem;
+        width: 100%;
+        height: 0.2rem;
+        z-index: 200;
+        background-color: black;
+        opacity: 0.6;
+      }
+
+      .hamburger > div:after {
+        top: 0.6rem;
+      }
+
+      .toggler:checked + .hamburger > div {
+        transform: rotate(135deg);
+      }
+
+      .toggler:checked + .hamburger > div:before,
+      .toggler:checked + .hamburger > div:after {
+        top: 0;
+        transform: rotate(90deg);
+      }
+
+      .toggler:checked:hover + .hamburger > div {
+        transform: rotate(225deg);
+      }
+
+      .menu {
+        position: fixed;
+        top: 4rem;
+        right: 3rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-top: 3rem;
+        padding-right: 2rem;
+        // padding-left: 2rem;
+        background-color: #f5f9f5;
+        // opacity: 0;
+        visibility: hidden;
+
+        ul {
+          list-style: none;
+          li {
+            padding-top: 2rem;
+
+            a {
+              text-decoration: none;
+              color: black;
+              opacity: 0.6;
+              transition: opacity 1s;
+            }
+          }
+        }
+
+        a:hover {
+          color: black;
+          opacity: 1;
+          transition: opacity 1s;
+        }
+      }
+
+      .toggler:checked ~ .menu {
+        visibility: visible;
+      }
+    }
+
+    .mobile-nav-item {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 15px 0;
+      text-align: center;
+      background: white;
+      color: black;
+      border-bottom: solid 0.0625rem #000000;
+      z-index: 100;
+    }
   }
 
   .bottom-section {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     border-top: none;
+
+    @media screen and (max-width: 900px) {
+      display: none;
+    }
   }
 
   .product-title {
@@ -157,6 +323,10 @@ export default {
     text-decoration: none;
     padding: 0.6rem 0;
     color: #1d1d1d;
+
+    @media screen and (max-width: 900px) {
+      border-bottom: solid 0.0625rem #000000;
+    }
   }
 
   .nav-item {
@@ -234,6 +404,20 @@ export default {
     background: #000000;
     color: white;
     transition: 0.5s;
+  }
+
+  .desktop-view {
+    @media screen and (max-width: 900px) {
+      display: none;
+    }
+  }
+
+  .mobile-view {
+    display: none;
+
+    @media screen and (max-width: 900px) {
+      display: flex;
+    }
   }
 }
 </style>

863121ccb691dedd0cb4696d946856bdba5ff983
lavikara <karatemitayo@gmail.com>
2020-12-02 05:07:31 +0100

    chore: removed unwanted code 🧹 🧹

--- src/main.js
+++ src/main.js
@@ -8,11 +8,7 @@ import sweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 import { Pagination, Icon, Select, Option } from "element-ui";
 import "@/assets/styles/scss/element-ui.scss";
-import SkeletonLine from "./components/Partials/skeleton/skeletonLine"
-import SkeletonCircle from './components/Partials/skeleton/skeletonCircle'
-import SkeletonSquare from './components/Partials/skeleton/skeletonSquare'
-import SkeletonBody from './components/Partials/skeleton/skeletonBody'
-import { store } from "./store";
+import store from "./store";
 
 Vue.use(VModal);
 Vue.use(sweetalert2);
@@ -21,25 +17,20 @@ Vue.use(Icon);
 Vue.use(Select);
 Vue.use(Option);
 Vue.config.productionTip = false;
-Vue.filter('shortenString',(value) =>{
-  if(value.length > 65){
-    return value.slice(0, 65) + '...'
-  }else{
-    return value
+Vue.filter("shortenString", (value) => {
+  if (value.length > 65) {
+    return value.slice(0, 65) + "...";
+  } else {
+    return value;
   }
-})
-Vue.filter('setUppercase',(value) =>{
-  return value[0].toUpperCase() + value.slice(1).toLowerCase()
-})
-
-Vue.component('SkeletonLine', SkeletonLine)
-Vue.component('SkeletonCircle', SkeletonCircle)
-Vue.component('SkeletonSquare', SkeletonSquare)
-Vue.component('SkeletonBody', SkeletonBody)
+});
+Vue.filter("setUppercase", (value) => {
+  return value[0].toUpperCase() + value.slice(1).toLowerCase();
+});
 
 new Vue({
   el: "#app",
   store,
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 });

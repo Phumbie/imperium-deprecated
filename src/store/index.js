d31edb395188d751f783ccb53ed7261006342fb8
lavikara <karatemitayo@gmail.com>
2020-12-08 13:00:33 +0100

    chore: deleted searchProductModule

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,6 @@ import * as getters from "./getters";
 import * as mutations from "./mutations";
 import * as actions from "./actions";
 import productModule from "./productModule/index";
-import searchProductModule from "./searchProductModule";
 
 export default new Vuex.Store({
   state,
@@ -17,6 +16,5 @@ export default new Vuex.Store({
   actions,
   modules: {
     productModule,
-    searchProductModule,
   },
 });

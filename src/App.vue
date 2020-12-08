88374c614c75b44ecebd418e0950f64ac87fc095
Phumbie <39763573+Phumbie@users.noreply.github.com>
2020-12-08 14:08:44 +0100

    created the modal component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <!-- <transition name="show"> -->
     <Toast />
-    <!-- </transition> -->
+    <!-- <Modal :showModal="show" @closeModal="show = false">
+      <h1>I'm a modal</h1>
+    </Modal> -->
     <router-view />
   </div>
 </template>
@@ -10,50 +11,19 @@
 <script>
   import Toast from "@/components/Notification/Toast/Toast";
   export default {
-    components: { Toast },
+    components: {
+      Toast,
+    },
     name: "App",
     data() {
       return {
+        // show: true,
         // showToast: false,
       };
     },
-    // watch: {
-    //   showToast(val) {
-    //     if (this.showToast === true) {
-    //       setTimeout(() => {
-    //         return this.$store.dispatch("notificationModule/showToast", {
-    //           description: "",
-    //           display: false,
-    //           type: "",
-    //         });
-    //       }, 2000);
-    //     }
-    //   },
-    // },
-    // computed: {
-    //   showToast: {
-    //     get() {
-    //       return this.$store.state.notificationModule.toast.display;
-    //     },
-    //     set(newValue) {
-    //       return this.$store.dispatch("notificationModule/showToast", newValue);
-    //     },
-    //   },
-    // },
   };
 </script>
 
 <style lang="scss">
   @import "@/assets/styles/scss/main.scss";
-
-  // .show-enter-active {
-  //   transition: all 0.3s ease-in;
-  // }
-  // .show-leave-active {
-  //   transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
-  // }
-  // .show-enter, .show-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  //   transform: translateY(-100px);
-  //   opacity: 0;
-  // }
 </style>

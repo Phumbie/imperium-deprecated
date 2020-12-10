0911a7f8816fb59a1a6c0dcfbd017ff7c5f1b45b
lavikara <karatemitayo@gmail.com>
2020-12-10 11:51:29 +0100

    chore: changed notification module folder name

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ import * as getters from "./getters";
 import * as mutations from "./mutations";
 import * as actions from "./actions";
 import productModule from "./productModule/index";
-import notificationModule from "./NotificationModule/index";
+import notificationModule from "./notificationModule/index";
 
 export default new Vuex.Store({
   state,

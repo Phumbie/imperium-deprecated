d888c10b6b01704d5f2ca214e60cdab4582b239a
lavikara <karatemitayo@gmail.com>
2020-12-10 17:18:01 +0100

    chore: added notification modal to app file

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <Toast />
-    <!-- <Modal :showModal="show" @closeModal="show = false">
-      <h1>I'm a modal</h1>
-    </Modal> -->
+    <Modal />
     <router-view />
   </div>
 </template>
 
 <script>
 import Toast from "@/components/Notification/Toast/Toast";
+import Modal from "@/components/Notification/Modal/Modal";
+
 export default {
   name: "App",
   components: {
     Toast,
+    Modal,
   },
 };
 </script>

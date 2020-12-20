1a510cc4cd60964ad4ff4e289f55a1f2e0d0cdf9
lavikara <karatemitayo@gmail.com>
2020-12-20 10:48:59 +0100

    chore: movd stae to store, changed email input type from text to email

--- src/components/Accounts/Login.vue
+++ src/components/Accounts/Login.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="login-section">
-    <form @submit.prevent="login()">
+    <form @submit.prevent="login">
       <div class="header-text-28">Login</div>
       <input
-        type="text"
+        type="email"
         placeholder="Email address"
         v-model="details.email"
         required
@@ -30,96 +30,32 @@
 </template>
 
 <script>
-  import { mapActions, mapState } from "vuex";
-  import api from "@/utils/api.js";
-  import contentLoader from "@/components/contentLoader";
+import { mapActions, mapState } from "vuex";
+import api from "@/utils/api.js";
+import contentLoader from "@/components/contentLoader";
 
-  export default {
-    name: "Login",
-    data() {
-      return {
-        details: {
-          email: "",
-          password: "",
-        },
-      };
+export default {
+  name: "Login",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState({
+      loading: (state) => state.loading,
+      details: (state) => state.accountModule.loginDetails,
+    }),
+  },
+  methods: {
+    ...mapActions("accountModule", ["loginCustomer"]),
+    navigateTo(page) {
+      this.$router.push(page);
     },
-    computed: {
-      ...mapState({
-        loading: (state) => state.loading,
-      }),
-    },
-    methods: {
-      ...mapActions("notificationModule", ["showModal"]),
-      ...mapActions("accountModule", ["loginCustomer"]),
-      navigateTo(page) {
-        this.$router.push(page);
-      },
 
-      login() {
-        this.loginCustomer(this.details);
-        //   this.loading = false;
-        //   let data = {
-        //     email: this.email,
-        //     password: this.password,
-        //   };
-        //   api
-        //     .loginCustomer(this.details)
-        //     .then((response) => {
-        //       if (response.data.status == "success") {
-        //         localStorage.setItem(
-        //           "user_details",
-        //           JSON.stringify(response.data.data)
-        //         );
-        //         localStorage.setItem("token", response.data.data.token);
-        //         this.navigateTo("my-account");
-        //         this.loading = true;
-        //       }
-        //       if (JSON.parse(localStorage.getItem("product_id")).length !== 0) {
-        //         let localCart = JSON.parse(localStorage.getItem("product_id"));
-        //         localCart.map((product) => {
-        //           product.product_id = product.id;
-        //           delete product.subtotal;
-        //           delete product.id;
-        //         });
-        //         const payload = { products: localCart };
-        //         api
-        //           .addBulkProductToCart(payload)
-        //           .then((response) => {
-        //             if (response) {
-        //               api
-        //                 .getCart()
-        //                 .then(({ data }) => {
-        //                   if (data.status == "success") {
-        //                     let cartSize = 0;
-        //                     let cartItems = data.data.cart.items;
-        //                     cartItems.forEach((item) => {
-        //                       cartSize += item.quantity;
-        //                     });
-        //                     this.$store.dispatch("setCartCounter", cartSize);
-        //                   }
-        //                 })
-        //                 .catch((error) => {
-        //                   alert(error.data.data.message);
-        //                 });
-        //             }
-        //           })
-        //           .catch((error) => {
-        //             alert(error.data.data.message);
-        //           });
-        //       }
-        //     })
-        //     .catch((error) => {
-        //       this.showModal({
-        //         description: "Invalid account.",
-        //         display: true,
-        //         type: "error",
-        //       });
-        //       this.loading = true;
-        //     });
-      },
+    login() {
+      this.loginCustomer(this.details);
     },
-  };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>

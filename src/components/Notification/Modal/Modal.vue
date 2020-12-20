76db54099d050e313ebe22f3478d524b13d5c394
lavikara <karatemitayo@gmail.com>
2020-12-20 10:55:08 +0100

    chore: prettier formated code

--- src/components/Notification/Modal/Modal.vue
+++ src/components/Notification/Modal/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fade">
-    <div class="modal" v-if="display" @click="clickTarget()">
+    <div class="modal" v-if="display" @click="clickTarget">
       <div class="modal-card">
         <div class="modal-card__close">
           <img
@@ -37,107 +37,107 @@
 </template>
 
 <script>
-  import { mapState, mapActions } from "vuex";
+import { mapState, mapActions } from "vuex";
 
-  export default {
-    name: "Modal",
-    data() {
-      return {};
-    },
-    computed: {
-      ...mapState("notificationModule", {
-        alert: (state) => state.modal.type,
-        description: (state) => state.modal.description,
-        display: (state) => state.modal.display,
-      }),
-    },
+export default {
+  name: "Modal",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState("notificationModule", {
+      alert: (state) => state.modal.type,
+      description: (state) => state.modal.description,
+      display: (state) => state.modal.display,
+    }),
+  },
 
-    methods: {
-      ...mapActions("notificationModule", ["showModal"]),
-      closeModal() {
-        this.showModal({
-          description: "",
-          display: false,
-          type: "",
-        });
-      },
-      clickTarget() {
-        if (event.target.className === "modal") {
-          this.closeModal();
-        }
-      },
+  methods: {
+    ...mapActions("notificationModule", ["showModal"]),
+    closeModal() {
+      this.showModal({
+        description: "",
+        display: false,
+        type: "",
+      });
     },
-  };
+    clickTarget() {
+      if (event.target.className === "modal") {
+        this.closeModal();
+      }
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-  .modal {
+.modal {
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  top: 0;
+  z-index: 1000;
+
+  .modal-card {
     position: fixed;
-    height: 100vh;
-    width: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    top: 0;
-    z-index: 1000;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    min-width: 20rem;
+    padding: 1rem;
+    background-color: #ffffff;
+    border-radius: 5px;
+    -webkit-box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    -moz-box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
 
-    .modal-card {
-      position: fixed;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      min-width: 20rem;
-      padding: 1rem;
-      background-color: #ffffff;
-      border-radius: 5px;
-      -webkit-box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
-      -moz-box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
-      box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    @media screen and (max-width: 600px) {
+      min-width: 80%;
+    }
 
-      @media screen and (max-width: 600px) {
-        min-width: 80%;
+    &__close {
+      position: absolute;
+      top: 0.5rem;
+      right: 0.5rem;
+      display: flex;
+      justify-content: flex-end;
+
+      img {
+        cursor: pointer;
       }
+    }
 
-      &__close {
-        position: absolute;
-        top: 0.5rem;
-        right: 0.5rem;
+    &__content {
+      display: flex;
+      margin: 1rem auto 1rem auto;
+
+      .icons {
         display: flex;
-        justify-content: flex-end;
+        justify-content: center;
 
         img {
-          cursor: pointer;
+          width: 3rem;
         }
       }
 
-      &__content {
-        display: flex;
-        margin: 1rem auto 1rem auto;
-
-        .icons {
-          display: flex;
-          justify-content: center;
-
-          img {
-            width: 3rem;
-          }
-        }
-
-        p {
-          max-width: 15rem;
-          color: #000000;
-          font-family: Graphik;
-          font-size: 1.2rem;
-          line-height: 1.6rem;
-          // text-align: center;
-          margin: auto;
-        }
+      p {
+        max-width: 15rem;
+        color: #000000;
+        font-family: Graphik;
+        font-size: 1.2rem;
+        line-height: 1.6rem;
+        // text-align: center;
+        margin: auto;
       }
     }
   }
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: all 0.3s cubic-bezier(0.45, 0.25, 0.6, 0.95);
-  }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    transform: translate(0%, -10px);
-    opacity: 0;
-  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s cubic-bezier(0.45, 0.25, 0.6, 0.95);
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  transform: translate(0%, -10px);
+  opacity: 0;
+}
 </style>

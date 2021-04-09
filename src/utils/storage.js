1751494211e81656824d4fb2ac62a08646abd83e
lavikara <karatemitayo@gmail.com>
2021-04-09 06:03:40 +0100

    chore: moved locl storage methods to storage file

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -2,11 +2,20 @@ export default {
   setUser(data) {
     return localStorage.setItem("user_details", JSON.stringify(data));
   },
+  getUser() {
+    return localStorage.getItem("user_details");
+  },
   setToken(data) {
     return localStorage.setItem("token", data);
   },
+  setStorageCart(data) {
+    return localStorage.setItem("product_id", JSON.stringify(data));
+  },
   getStorageCart() {
     return JSON.parse(localStorage.getItem("product_id"));
+  },
+  setCartCount(data) {
+    return localStorage.setItem("cartCounter", JSON.stringify(data));
   },
   getCartCount() {
     return JSON.parse(localStorage.getItem("cartCounter"));
